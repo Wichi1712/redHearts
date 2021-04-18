359c3bbaa879031336c928e777646e1c97ef41f5
Wichi1712 <46914107+Wichi1712@users.noreply.github.com>
2021-04-18 07:28:17 -0500

    Add files via upload

---
+++ libs.lua
@@ -0,0 +1,28 @@
+function distanceBetween(x1, y1, x2, y2)
+    return math.sqrt((y2 - y1)^2 + (x2 - x1)^2)
+end
+
+function playerMouseAngle()
+    return math.atan2(player.y - love.mouse.getY(), player.x - love.mouse.getX()) + math.pi
+end
+
+function galMouseAngle(enemy)
+    return math.atan2(y - enemy.y, x - enemy.x)
+end
+
+music = love.audio.newSource('music/topcity.ogg', "stream")
+music:setLooping(true)
+--[[Para reproducir el sonido se puede usar cualquiera de las
+las siguientes funciones]]
+music:play()
+--love.audio.play(music)
+
+shoot = love.audio.newSource('music/shoot.wav', "static")
+
+WIDTH = love.graphics.getWidth()
+HEIGHT = love.graphics.getHeight()
+background = love.graphics.newImage('sprites/background.png')
+
+
+font = love.graphics.newFont('fonts/ComputerSpeak.ttf', 24)
+love.graphics.setFont(font)
